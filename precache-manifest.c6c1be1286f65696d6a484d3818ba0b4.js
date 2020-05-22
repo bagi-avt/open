505f08c4d598ae76d16b3716858b6d712b13fce2
Abdumutallov <abdumutallov@gmail.com>
2020-05-22 22:22:47 +0700

    Updates

---
+++ precache-manifest.c6c1be1286f65696d6a484d3818ba0b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898d3fc4142347170c1897c15c8e5cdd",
+    "url": "/open/index.html"
+  },
+  {
+    "revision": "c5f72c9913ac02ac1f2f",
+    "url": "/open/static/css/2.a971a1bc.chunk.css"
+  },
+  {
+    "revision": "19acb6b0dccda412ce01",
+    "url": "/open/static/css/main.759ef44f.chunk.css"
+  },
+  {
+    "revision": "c5f72c9913ac02ac1f2f",
+    "url": "/open/static/js/2.c1b722bd.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/open/static/js/2.c1b722bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19acb6b0dccda412ce01",
+    "url": "/open/static/js/main.379ad307.chunk.js"
+  },
+  {
+    "revision": "ae373a86907b0ec5bc78",
+    "url": "/open/static/js/runtime-main.3a6ad94f.js"
+  },
+  {
+    "revision": "575c9f65fe89c821615ca59de975f51e",
+    "url": "/open/static/media/logo.575c9f65.svg"
+  }
+]);
